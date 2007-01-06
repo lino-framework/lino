da3f5d52836d0d411df4ffd7e048051da3109d2b
lsaffre <devnull@localhost>
2007-01-06 13:44:53 +0000

    http://lino.saffre-rumma.ee/news/468.html

---
+++ docs/help/makethem.py
@@ -0,0 +1,10 @@
+from lino.apps.timtools import console_targets
+from lino.console.syscon import confirm
+from lino.misc.tsttools import trycmd
+
+if confirm("Are you sure?"):
+    for ct in console_targets():
+        cmd="lino %s --help > %s.help.txt" % (ct,ct)
+        print cmd
+        trycmd(cmd)
+    
