88a382495de0104b48791f853249be91b6f74e39
lsaffre <devnull@localhost>
2009-03-11 22:00:41 +0000

    m

--- src/mysites/demo/settings.py
+++ src/mysites/demo/settings.py
@@ -34,7 +34,7 @@ MANAGERS = ADMINS
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 
 #DATABASE_NAME = ':memory:'
-DATABASE_NAME = os.path.join(gettempdir(),'mysites_sandbox.db') 
+DATABASE_NAME = os.path.join(gettempdir(),'mysites_demo.db') 
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
