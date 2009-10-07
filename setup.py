3e9e8e826dad7e1c10f0214b40893f4cf44f4f88
Luc Saffre <luc.saffre@mail.ee>
2009-10-06 23:22:16 -0300

    register to pypi using setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
 from distutils.core import setup, Extension
-raise "not tested"
+import lino
 setup(name='lino',
-      version='0.6.14',
-      description="The universal framework for application development",
+      version=lino.__version__,
+      description="A web application framework using Django and ExtJS",
       license='GPL',
-      py_modules=['lino'],
-      author='Luc saffre',
-      author_email='lucs.saffre@gmx.net',
+      packages=['lino','lino.apps','lino.utils'],
+      author='Luc Saffre',
+      author_email='luc.saffre@gmail.com',
+      requires=['django'],
+      url="http://lino.saffre-rumma.ee",
+      classifiers="""\
+Development Status :: 2 - Pre-Alpha
+Environment :: Web Environment
+Framework :: Django
+Intended Audience :: Developers
+Intended Audience :: System Administrators
+License :: OSI Approved :: GNU General Public License (GPL)
+Natural Language :: French
+Natural Language :: German
+Operating System :: OS Independent
+Programming Language :: Python :: 2
+Topic :: Database :: Front-Ends
+Topic :: Home Automation
+Topic :: Office/Business
+Topic :: Software Development :: Libraries :: Application Frameworks
+""".splitlines()
       )
