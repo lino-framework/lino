93e5f1567eeb8a1ae3ec85f4a86896f9f41f0e6c
Luc Saffre <luc.saffre@mail.ee>
2011-03-03 06:22:48 -0300

    20110303c

--- lino/management/commands/initdb.py
+++ lino/management/commands/initdb.py
@@ -12,6 +12,7 @@
 ## You should have received a copy of the GNU General Public License
 ## along with TimTools; if not, see <http://www.gnu.org/licenses/>.
 
+import logging
 from optparse import make_option 
 
 from django.conf import settings
