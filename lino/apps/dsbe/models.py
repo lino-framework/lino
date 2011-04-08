c0d1e3e4ecf20efa83eb8863d47d99abd2373908
Luc Saffre <luc.saffre@mail.ee>
2011-04-08 01:59:03 -0300

    20110408

--- lino/apps/dsbe/models.py
+++ lino/apps/dsbe/models.py
@@ -619,9 +619,9 @@ def only_coached_persons(qs,period_from,period_until=None):
     period_until = period_until or period_from
     #~ today = datetime.date.today()
     if period_from is not None:
-        qs = qs.filter(coached_until__isnull=False,coached_until__gte=period_from)
+        qs = qs.filter(models.Q(coached_until__isnull=False,coached_until__gte=period_from))
     if period_until is not None:
-        qs = qs.filter(coached_from__isnull=False,coached_from__lte=period_until)
+        qs = qs.filter(models.Q(coached_from__isnull=False,coached_from__lte=period_until))
     return qs
     #~ return qs.filter(
         #~ models.Q(coached_from__isnull=False,coached_from__lte=period_until) | 
