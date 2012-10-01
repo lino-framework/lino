6fae8cd7e4a6516fe2b1a8623c541bfec7361e8f
Luc Saffre <luc.saffre@mail.ee>
2012-10-01 04:39:37 -0400

    http://code.google.com/p/lino/source/browse/docs/blog/2012/1001.rst

--- lino/models.py
+++ lino/models.py
@@ -514,7 +514,7 @@ class Inspector(dd.VirtualTable):
             pv.update(inspected=obj.name)
         #~ newreq = ar.spawn(ar.ui,user=ar.user,renderer=ar.renderer,param_values=pv)
         newreq = ar.spawn(param_values=pv)
-        return ar.renderer.href_to_request(newreq,obj.name)
+        return ar.href_to_request(newreq,obj.name)
         #~ return obj.name
         
     @dd.displayfield(_("Value"))
