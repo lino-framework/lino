4991076579ef78c160051eeee6daaa27848a11a9
Luc Saffre <luc.saffre@mail.ee>
2012-11-22 05:35:53 -0400

    http://code.google.com/p/lino/source/browse/docs/blog/2012/1122.rst

--- lino/modlib/users/fixtures/demo.py
+++ lino/modlib/users/fixtures/demo.py
@@ -32,7 +32,7 @@ def root_user(lang,**kw):
     if lang == 'de':
         kw.update(first_name="Rudi",last_name=u"Rutté")
     elif lang == 'fr':
-        kw.update(first_name=u"Romain",last_name=u"Rouvier")
+        kw.update(first_name=u"Romain",last_name=u"Raffault")
     elif lang == 'et':
         kw.update(first_name="Rando",last_name=u"Roosi")
     elif lang == 'en':
