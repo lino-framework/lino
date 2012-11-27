1e5d99c28acb4932f45a0ce16a897d4f63d660b9
Luc Saffre <luc.saffre@mail.ee>
2012-11-27 00:26:27 -0400

    http://code.google.com/p/lino/source/browse/docs/blog/2012/1127.rst

--- lino/management/commands/monitor.py
+++ lino/management/commands/monitor.py
@@ -148,7 +148,7 @@ class Command(BaseCommand):
                     FROM information_schema.tables
                     WHERE table_schema='%s' and table_name='%s';
                     """ % (connection.alias,model._meta.db_table)
-                    #~ cursur.execute(sql)
+                    cursur.execute(sql)
                     row = cursor.fetchone()
                     #~ transaction.commit_unless_managed(using='my_db_alias')
                     #~ cells.append(str(row))
