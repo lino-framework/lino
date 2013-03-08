a77b19d9f50d999ce05356ae7d1bd52316357b3c
Luc Saffre <luc.saffre@gmx.net>
2013-03-08 06:57:22 -0400

    http://code.google.com/p/lino/source/browse/docs/blog/2013/0308.rst

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-#~ import os
 import setuptools
-#~ execfile(os.path.join('lino','setup_info.py'))
 execfile('lino/setup_info.py')
 def main(): setuptools.setup(**SETUP_INFO)
 if __name__ == "__main__": main()
