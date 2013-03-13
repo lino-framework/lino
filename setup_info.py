74778932665d29c31a95db9dc7d3ce01161fede2
Luc Saffre <luc.saffre@gmx.net>
2013-03-13 06:14:31 -0400

    http://code.google.com/p/lino/source/browse/docs/blog/2013/0313.rst

--- setup_info.py
+++ setup_info.py
@@ -14,10 +14,135 @@
 #~ """
 #~ __version__ = 
 
+
+PACKAGES = [n for n in """
+lino
+lino.core
+lino.history
+lino.sandbox.bcss
+lino.sandbox.contacts
+lino.sandbox.contacts.fixtures
+lino.sandbox.debts
+lino.sandbox.debts.fixtures
+lino.modlib
+lino.modlib.contacts
+lino.modlib.contacts.fixtures
+lino.modlib.countries
+lino.modlib.countries.fixtures
+lino.modlib.finan
+lino.modlib.finan.fixtures
+lino.modlib.ledger
+lino.modlib.ledger.fixtures
+lino.modlib.links
+lino.modlib.links.fixtures
+lino.modlib.links.tests
+lino.modlib.notes
+lino.modlib.notes.fixtures
+lino.modlib.products
+lino.modlib.products.fixtures
+lino.modlib.projects
+lino.modlib.sales
+lino.modlib.sales.fixtures
+lino.modlib.uploads
+lino.modlib.properties
+lino.modlib.users
+lino.modlib.thirds
+lino.modlib.cal
+lino.modlib.cal.management
+lino.modlib.cal.management.commands
+lino.modlib.cal.fixtures
+lino.modlib.cal.tests
+lino.modlib.tickets
+lino.modlib.tickets.fixtures
+lino.modlib.vocbook
+lino.modlib.outbox
+lino.modlib.outbox.fixtures
+lino.modlib.blogs
+lino.modlib.workflows
+lino.modlib.postings
+lino.modlib.accounts
+lino.modlib.vat
+lino.modlib.households
+lino.modlib.households.fixtures
+lino.modlib.school
+lino.modlib.school.fixtures
+lino.modlib.about
+lino.modlib.pages
+lino.modlib.pages.fixtures
+lino.modlib.codechanges
+lino.modlib.orders
+lino.modlib.changes
+lino.modlib.concepts
+lino.modlib.partners
+lino.modlib.partners.fixtures
+lino.modlib.declarations
+lino.modlib.events
+lino.modlib.events.fixtures
+lino.modlib.events.tests
+lino.test_apps
+lino.test_apps.20090714
+lino.test_apps.20090717
+lino.test_apps.20100126
+lino.test_apps.20100127
+lino.test_apps.20100206
+lino.test_apps.20100212
+lino.test_apps.chooser
+lino.test_apps.properties
+lino.test_apps.example
+lino.test_apps.quantityfield
+lino.test_apps.20121124
+lino.test_apps.mti
+lino.test_apps.nomti
+lino.utils
+lino.utils.xmlgen
+lino.utils.xmlgen.intervat
+lino.utils.xmlgen.cbss
+lino.utils.xmlgen.odf
+lino.tutorials
+lino.tutorials.mini
+lino.tutorials.lets1
+lino.tutorials.lets1.fixtures
+lino.tutorials.lets1.lets
+lino.management
+lino.management.commands
+lino.mixins
+lino.ui
+lino.ui.fixtures
+lino.ui.tests
+lino.projects
+lino.projects.belref
+lino.projects.belref.fixtures
+lino.projects.cms
+lino.projects.cms.fixtures
+lino.projects.cosi
+lino.projects.cosi.fixtures
+lino.projects.crl
+lino.projects.crl.fixtures
+lino.projects.events
+lino.projects.homeworkschool
+lino.projects.homeworkschool.fixtures
+lino.projects.igen
+lino.projects.igen.tests
+lino.projects.min1
+lino.projects.min2
+lino.projects.polls_tutorial
+lino.projects.polls_tutorial.polls
+lino.projects.polls_tutorial.polls.fixtures
+lino.projects.presto
+lino.projects.presto.fixtures
+lino.projects.std
+lino.projects.uiless.fixtures
+lino.projects.uiless.mysite
+lino.projects.uiless.polls
+lino.projects.uiless.polls.fixtures
+lino.projects.babel_tutorial
+lino.projects.babel_tutorial.fixtures
+""".splitlines() if n]
+  
 SETUP_INFO = dict(name = 'lino',
-  version = '1.6.1',
-  requires = ['North(==0.0.3)','appy',
-    'python_dateutil','PyYAML','odfpy','sphinx','jinja2',
+  version = '1.6.2',
+  requires = ['North(==0.0.4)','appy',
+    'python_dateutil','PyYAML','odfpy','jinja2',
     ],
   description = "A framework for writing desktop-like web applications using Django and ExtJS",
   long_description = """\
@@ -50,7 +175,7 @@ The following real-world applications use the Lino framework:
   
   """,
   license = 'GPL',
-  #~ packages = ['lino' ],
+  packages = PACKAGES,
   author = 'Luc Saffre',
   author_email = 'luc.saffre@gmail.com',
   url = "http://www.lino-framework.org",
