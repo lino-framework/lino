2e26c56067bc02f985206faf75d61150e4f1346c
Luc Saffre <luc.saffre@gmx.net>
2013-08-13 23:12:07 +0300

    http://code.google.com/p/lino/source/browse/docs/blog/2013/0813.rst

--- lino/setup_info.py
+++ lino/setup_info.py
@@ -1,6 +1,6 @@
 SETUP_INFO = dict(name = 'lino',
   version = '1.6.11',
-  install_requires = ['North==0.1.5','djangosite>=0.1.5','PyYAML','odfpy','jinja2','appy'],
+  install_requires = ['North','djangosite','PyYAML','odfpy','jinja2','appy'],
   description = "A framework for writing desktop-like web applications using Django and ExtJS",
   license = 'GPL',
   include_package_data = True,
