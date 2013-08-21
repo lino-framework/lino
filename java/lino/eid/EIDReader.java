c53eead215a0a975ab091b85fbe0e3bc21f68606
Luc Saffre <luc.saffre@gmx.net>
2013-08-21 22:16:16 +0300

    http://code.google.com/p/lino/source/browse/docs/blog/2013/0821.rst

--- java/lino/eid/EIDReader.java
+++ java/lino/eid/EIDReader.java
@@ -27,9 +27,17 @@ import java.security.Permission;
 
 import java.io.FilePermission;
 import java.io.UnsupportedEncodingException;  
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 import java.util.Arrays;  
 import java.util.List; 
+
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 
 import javax.smartcardio.CardPermission;  
 import javax.smartcardio.CardChannel;  
@@ -102,7 +110,7 @@ class PersonalFile {
   
   @Override  
   public String toString() {  
-    String s = "";
+    String s = "reader:EST\n";
     for (byte i = 0; i < 16; i++) {  
         s = s + fields[i] + ":" + data[i].trim() + "\n";
     }
@@ -126,6 +134,175 @@ class PersonalFile {
   }  
 }  
 
+class BelgianReader {
+    public static final byte FIELD_COUNT = 16;
+    private CardChannel cardChannel;
+    String[] data = new String[FIELD_COUNT];
+    public static final String[] fields = new String[] {
+        "last_name","first_name", "other_names", "gender", "nationality",
+        "birth_date","national_id","card_id","valid_until",
+        "birth_place", "date_issued", "ResidencePermitType",
+        "remark1",
+        "remark2",
+        "remark3",
+        "remark4"
+        };
+    
+	private static final int BLOCK_SIZE = 0xff;
+    
+	private final static byte[] ATR_PATTERN = new byte[] { 0x3b, (byte) 0x98,
+			0x00, 0x40, 0x00, (byte) 0x00, 0x00, 0x00, 0x01, 0x01, (byte) 0xad,
+			0x13, 0x10 };
+
+	private final static byte[] ATR_MASK = new byte[] { (byte) 0xff,
+			(byte) 0xff, 0x00, (byte) 0xff, 0x00, 0x00, 0x00, 0x00,
+			(byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xf0 };
+
+	public static final byte[] IDENTITY_FILE_ID = new byte[] { 0x3F, 0x00,
+			(byte) 0xDF, 0x01, 0x40, 0x31 };
+
+	public static final byte[] ADDRESS_FILE_ID = new byte[] { 0x3F, 0x00,
+			(byte) 0xDF, 0x01, 0x40, 0x33 };
+
+	public static final byte[] PHOTO_FILE_ID = new byte[] { 0x3F, 0x00,
+			(byte) 0xDF, 0x01, 0x40, 0x35 };
+
+	public static boolean matchesEidAtr(ATR atr) { // from eid-applet-core...PcscEid.java
+		byte[] atrBytes = atr.getBytes();
+		if (atrBytes.length != ATR_PATTERN.length) {
+			return false;
+		}
+		for (int idx = 0; idx < atrBytes.length; idx++) {
+			atrBytes[idx] &= ATR_MASK[idx];
+		}
+		if (Arrays.equals(atrBytes, ATR_PATTERN)) {
+			return true;
+		}
+		return false;
+	}
+    
+    public BelgianReader(CardChannel channel) 
+        throws CardException, IOException, UnsupportedEncodingException 
+    {  
+        this.cardChannel = channel;
+        for (byte i = 0; i < FIELD_COUNT; i++) {  
+            data[i] = "";
+        }
+        
+        
+        //~ byte[] photoFile = readFile(PHOTO_FILE_ID);
+        //~ BufferedImage photo = ImageIO.read(new ByteArrayInputStream(photoFile));
+        
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        
+		byte[] identityFile = readFile(IDENTITY_FILE_ID);
+        System.err.println("identityFile.length is " + Integer.toString(identityFile.length));
+        baos.write(identityFile);
+        
+        //~ baos.write('\n');
+        
+		byte[] addressFile = readFile(ADDRESS_FILE_ID);
+        baos.write(addressFile);
+        System.err.println("addressFile.length is " + Integer.toString(addressFile.length));
+        
+        //~ data[0] = baos.toString("utf-8");
+        //~ data[0] = baos.toString("ISO-8859-1 ");
+        data[0] = baos.toString();
+    }  
+    
+	public byte[] readFile(byte[] fileId) throws CardException, IOException {
+		selectFile(fileId);
+		byte[] data = readBinary();
+		return data;
+	}
+    
+	private void selectFile(byte[] fileId) throws CardException,
+			FileNotFoundException {
+		CommandAPDU selectFileApdu = new CommandAPDU(0x00, 0xA4, 0x08, 0x0C,
+				fileId);
+		ResponseAPDU responseApdu = transmit(selectFileApdu);
+		if (0x9000 != responseApdu.getSW()) {
+			throw new FileNotFoundException(
+					"wrong status word after selecting file: "
+							+ Integer.toHexString(responseApdu.getSW()));
+		}
+		try {
+			// SCARD_E_SHARING_VIOLATION fix
+			Thread.sleep(20);
+		} catch (InterruptedException e) {
+			throw new RuntimeException("sleep error: " + e.getMessage());
+		}
+	}
+    
+	private byte[] readBinary() throws CardException, IOException {
+		int offset = 0;
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		byte[] data;
+		do {
+			CommandAPDU readBinaryApdu = new CommandAPDU(0x00, 0xB0,
+					offset >> 8, offset & 0xFF, BLOCK_SIZE);
+			ResponseAPDU responseApdu = transmit(readBinaryApdu);
+			int sw = responseApdu.getSW();
+			if (0x6B00 == sw) {
+				/*
+				 * Wrong parameters (offset outside the EF) End of file reached.
+				 * Can happen in case the file size is a multiple of 0xff bytes.
+				 */
+				break;
+			}
+			if (0x9000 != sw) {
+				throw new IOException("APDU response error: "
+						+ responseApdu.getSW());
+			}
+
+			/*
+			 * Introduce some delay for old Belpic V1 eID cards.
+			 */
+			// try {
+			// Thread.sleep(50);
+			// } catch (InterruptedException e) {
+			// throw new RuntimeException("sleep error: " + e.getMessage(), e);
+			// }
+			data = responseApdu.getData();
+			baos.write(data);
+			offset += data.length;
+		} while (BLOCK_SIZE == data.length);
+		return baos.toByteArray();
+	}
+
+    
+
+    
+
+	private ResponseAPDU transmit(CommandAPDU commandApdu) throws CardException {
+		ResponseAPDU responseApdu = this.cardChannel.transmit(commandApdu);
+		if (0x6c == responseApdu.getSW1()) {
+			/*
+			 * A minimum delay of 10 msec between the answer ‘6C xx’ and the
+			 * next APDU is mandatory for eID v1.0 and v1.1 cards.
+			 */
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				throw new RuntimeException("cannot sleep");
+			}
+			responseApdu = this.cardChannel.transmit(commandApdu);
+		}
+		return responseApdu;
+	}
+
+    
+    
+    @Override  
+    public String toString() {  
+      String s = "reader:BEL\n";
+      for (byte i = 0; i < FIELD_COUNT; i++) {  
+          s = s + fields[i] + ":" + data[i].trim() + "\n";
+      }
+      return s;
+    }  
+  
+}
 
 public class EIDReader extends Applet {
 //~ class EstEID {
@@ -159,9 +336,11 @@ public class EIDReader extends Applet {
         System.err.println("Initialized");
     }
     
-    public String readCard() {
+    public String readCard() 
+        throws CardException, IOException 
+    {
         
-        try {
+        //~ try {
             TerminalFactory factory = TerminalFactory.getDefault();  
             List<CardTerminal> terminals = factory.terminals().list();          
             CardTerminal terminal = terminals.get(0);  
@@ -172,20 +351,25 @@ public class EIDReader extends Applet {
             }
             
             Card card = terminal.connect("T=0");  
-            //~ ATR atr = card.getATR();
+            ATR atr = card.getATR();
+            
             CardChannel channel = card.getBasicChannel();  
+            
+            if (BelgianReader.matchesEidAtr(atr)) {
+                BelgianReader pf = new BelgianReader(channel);
+                return pf.toString();
+            }
+            
 
-            //~ EstEID a = new EstEID();
-            //~ a.init();
             PersonalFile pf = new PersonalFile(channel);
-            //~ a.readCard();
             return pf.toString();
             //~ return new String[] { pf.toString() };
             //~ return new EidReaderResponse(pf.getData());
             //~ return pf.getSurName();
-        } catch (Exception e) {
+        //~ } catch (Exception e) {
             //~ return new EidReaderResponse(new String[] { e.toString() });
-            return e.toString();
-        }
+            //~ return e.toString();
+        //~ }
     }
+    
 }
