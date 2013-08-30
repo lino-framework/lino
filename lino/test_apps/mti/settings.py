a4af5af1dc59f3dd434b111c8d5b835153c25650
Luc Saffre <luc.saffre@gmx.net>
2013-08-30 13:20:21 +0300

    http://code.google.com/p/lino/source/browse/docs/blog/2013/0830.rst

--- lino/test_apps/mti/settings.py
+++ lino/test_apps/mti/settings.py
@@ -1,13 +1,5 @@
 from lino.projects.std.settings import *
 #~ class Lino(Lino):
     #~ user_model = None
-SITE = Site(globals(),'lino','lino.test_apps.mti')
-
-#~ INSTALLED_APPS = (
-  #~ # 'django.contrib.contenttypes', 
-  #~ 'lino', 
-  #~ # 'lino.modlib.users', 
-  #~ # 'lino.modlib.contacts', 
-  #~ 'lino.test_apps.mti', # Lino and MTI
-#~ )
+SITE = Site(globals(),['lino','lino.test_apps.mti'])
 
