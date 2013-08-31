68e36b1e3845af964652dc330b1f327c40424f1e
Luc Saffre <luc.saffre@gmx.net>
2013-08-31 16:52:10 +0300

    http://code.google.com/p/lino/source/browse/docs/blog/2013/0831.rst

--- lino/core/requests.py
+++ lino/core/requests.py
@@ -530,7 +530,7 @@ class ActionRequest(BaseRequest):
             bp[ext_requests.URL_PARAM_PROJECT] = self.current_project
             
         if self.selected_rows is not None:
-            bp[ext_requests.URL_PARAM_SELECTED] = [obj.pk for obj in self.selected_rows]
+            bp[ext_requests.URL_PARAM_SELECTED] = [obj.pk for obj in self.selected_rows if obj is not None]
             
         if self.subst_user is not None:
             #~ bp[ext_requests.URL_PARAM_SUBST_USER] = self.subst_user.username
