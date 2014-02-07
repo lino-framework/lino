fb64eb4648176bbaf48c90cbabf47fd3530fa32e
Luc Saffre <luc.saffre@gmail.com>
2014-02-07 04:30:39 +0200

    http://docs.lino-framework.org/blog/2014/0207.html

--- docs/conf.py
+++ docs/conf.py
@@ -96,7 +96,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u"Lino"
-copyright = u'2002-2013, Luc Saffre'
+copyright = u'2002-2014 Luc Saffre'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
