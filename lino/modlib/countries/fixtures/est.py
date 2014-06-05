a2233ea48523ce98e951707a37e0c0adadca91f8
Luc Saffre <luc.saffre@gmail.com>
2014-06-06 02:02:43 +0300

    http://docs.lino-framework.org/blog/2014/0606.html

--- lino/modlib/countries/fixtures/est.py
+++ lino/modlib/countries/fixtures/est.py
@@ -12,6 +12,14 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Lino; if not, see <http://www.gnu.org/licenses/>.
 
+"""This fixture contains all Estonian places.
+
+It is a proof of concept for my idea of using :ref:`dpy` as data
+format for maintaining large knowledge bases like the list of places
+of a country.
+
+"""
+
 from __future__ import unicode_literals
 from lino.modlib.countries.utils import PlaceGenerator
 
