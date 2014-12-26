1274b6efc6230f544feaaa44589168a2cc13fc91
Luc Saffre <luc.saffre@gmail.com>
2014-12-26 06:38:27 +0200

    http://www.lino-framework.org/blog/2014/1226.html

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
 from atelier.fablib import *
-setup_from_project('lino_noi', 'lino_noi.settings.demo')
+setup_from_fabfile(globals(), 'lino_noi', 'lino_noi.settings.demo')
 
 env.revision_control_system = 'git'
