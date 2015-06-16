5d1b8fe657b2982c20f4fcb1f5d3a26e805d7ca5
Luc Saffre <luc.saffre@gmail.com>
2015-06-16 07:10:18 +0300

    http://luc.lino-framework.org/blog/2015/0616.html

--- lino_noi/project_info.py
+++ lino_noi/project_info.py
@@ -13,11 +13,11 @@ Lino Noi is the `Lino <http://www.lino-framework.org>`_
 application used by the Lino team for managing their work on the Lino project.
 The name comes from Italian "noi" which means "we".
 
-It can be called a <a
-href="https://en.wikipedia.org/wiki/DevOps">DevOps tool</a> in that it
-stresses communication, collaboration, integration, automation, and
-measurement of cooperation between software developers, users and
-other IT professionals.
+It can be called a `DevOps tool
+<https://en.wikipedia.org/wiki/DevOps>`_ in that it stresses
+communication, collaboration, integration, automation, and measurement
+of cooperation between software developers, users and other IT
+professionals.
 
 """,
     author='Luc Saffre',
