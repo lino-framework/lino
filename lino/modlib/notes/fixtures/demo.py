5f509430d07a513af1f8a2d4c86b799de36ab2ef
Luc Saffre <luc.saffre@gmail.com>
2015-08-18 03:07:52 +0300

    http://luc.lino-framework.org/blog/2015/0818.html

--- lino/modlib/notes/fixtures/demo.py
+++ lino/modlib/notes/fixtures/demo.py
@@ -2,38 +2,42 @@
 # Copyright 2009-2015 Luc Saffre
 # License: BSD (see file COPYING for details)
 
-"""Generate one houndred notes.
-
-TODO: this fixture fails if `settings.SITE.project_model` is empty or
-points to a model that has no `name` field.
+"""Generate some fictive notes.
 
 """
 
 from django.conf import settings
 
 from lino.utils.instantiator import Instantiator
-from lino.core.utils import resolve_model
 from lino.utils import Cycler
+
+from lino.api import rt, _
+
+SUBJECTS = Cycler((
+    _("Get acquaintaned"),
+    _("Ideas"),
+    _("Feedback after first working day"),
+    _("More ideas"),
+    _("Tried to explain"),
+    _("Appointment with RCycle"),
+    _("Cancelled"),
+    _("Apologized via SMS"),
+))
 
 
 def objects():
-    User = resolve_model(settings.SITE.user_model)
-    Note = resolve_model('notes.Note')
-    NoteType = resolve_model('notes.NoteType')
+    User = rt.modules.users.User
+    Note = rt.modules.notes.Note
+    NoteType = rt.modules.notes.NoteType
 
     USERS = Cycler(User.objects.all())
     if settings.SITE.project_model is not None:
-        Project = resolve_model(settings.SITE.project_model)
+        Project = settings.SITE.project_model
         qs = Project.objects.all()
         if qs.count() > 10:
             qs = qs[:10]
         PROJECTS = Cycler(qs)
-        #~ PROJECTS = Cycler(Project.objects.filter(name__startswith="A"))
-        #~ PROJECTS = Cycler(Project.objects.all())
-    #~ COMPANIES = Cycler(Company.objects.all())
     NTYPES = Cycler(NoteType.objects.all())
-
-    #~ u = User.objects.get(username='root')
 
     notetype = Instantiator('notes.NoteType').build
     tel = notetype(name="phone report")
@@ -43,8 +47,7 @@ def objects():
     for i in range(100):
         kw = dict(user=USERS.pop(),
                   date=settings.SITE.demo_date(days=i-400),
-                  subject="Important note %d" % i,
-                  #~ company=COMPANIES.pop(),
+                  subject=SUBJECTS.pop(),  # "Important note %d" % i,
                   type=NTYPES.pop())
         if settings.SITE.project_model is not None:
             kw.update(project=PROJECTS.pop())
