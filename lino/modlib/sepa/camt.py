52fe22d99596a60ee81607ab647c6b58f719d979
khchine5 <hamza.khchine@gmail.com>
2015-09-13 17:02:31 +0100

    Add links sources fo files for importing SEPA file , ticket 505.

--- lino/modlib/sepa/camt.py
+++ lino/modlib/sepa/camt.py
@@ -1,5 +1,24 @@
 # -*- coding: utf-8 -*-
 """Class to parse camt files."""
+##############################################################################
+#
+#    Copyright (C) 2013-2015 Therp BV <http://therp.nl>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+# File taken from https://github.com/OCA/bank-statement-import/blob/8.0/account_bank_statement_import_camt/camt.py
 
 import re
 from datetime import datetime
