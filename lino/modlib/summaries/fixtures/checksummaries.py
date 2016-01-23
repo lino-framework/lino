ef12ed568d7204ab533a4198bcb9bdb1106a9f8a
Luc Saffre <luc.saffre@gmail.com>
2016-01-23 04:14:39 +0200

    http://luc.lino-framework.org/blog/2016/0123.html

---
+++ lino/modlib/summaries/fixtures/checksummaries.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2016 Luc Saffre
+# License: BSD (see file COPYING for details)
+"""Runs the :manage:`checksummaries` management command.
+
+"""
+
+from django.core.management import call_command
+
+
+def objects():
+    call_command('checksummaries')
+    return []
---
+++ lino/modlib/summaries/management/commands/checksummaries.py
@@ -0,0 +1,43 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2016 by Luc Saffre.
+# License: BSD, see LICENSE for more details.
+"""Defines the :manage:`checksummaries` management command:
+
+.. management_command:: checksummaries
+
+
+.. py2rst::
+
+  from lino.modlib.summaries.management.commands.checksummaries \
+      import Command
+  print(Command.help)
+
+
+"""
+
+from __future__ import unicode_literals, print_function
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from lino.api import rt
+
+
+class Command(BaseCommand):
+    args = "[app1.Model1] [app2.Model2] ..."
+    help = """
+
+    Update the summary tables.
+
+    If no arguments are given, run it on all summaries.
+
+    Otherwise (not yet implemented) every positional argument is
+    expected to be a model name in the form `app_label.ModelName`, and
+    only these models are being updated.
+
+    """
+
+    def handle(self, *args, **options):
+        ses = rt.login()
+        ses.run(settings.SITE.site_config.check_summaries)
+        # checksummaries(args=args)
