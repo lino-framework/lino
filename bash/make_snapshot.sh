7c5e607582ee10281148b88ace3cb8185f4f0738
Luc Saffre <luc.saffre@gmail.com>
2016-05-25 10:53:01 +0300

    http://luc.lino-framework.org/blog/2016/0525.html

--- bash/make_snapshot.sh
+++ bash/make_snapshot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2015 Luc Saffre
+# Copyright 2015-2016 Luc Saffre
 # License: BSD (see file COPYING for details)
 #
 # Make a snapshot of a Lino database.
@@ -13,25 +13,41 @@ set -e  # exit on error
 # /etc/cron.d/lino_backup where cwd is not set:
 # cd /path/to/project/dir
 
+PROJECT_DIR=/path/to/myproject
+ARCH_DIR=/var/backups/lino/myproject
+
+if [ ! -d "$PROJET_DIR" ]; then
+  echo $PROJET_DIR does not exist!
+  exit -1
+fi
+
+# edit the following lines if you also want to include a MySQL dump
+MYSQL_USERNAME=
+MYSQL_PASSWORD=
+MYSQL_DBNAME=
+
 # Directory where to put the temporary snapshot files.
 # WARNING: everything in this directory will be deleted without confirmation
-SNAPSHOTDIR=snapshot
+SNAPSHOTDIR=$PROJECT_DIR/snapshot
 
 # name of target zip file to be created:
-ZIPFILE=snapshot.zip
+ZIPFILE=$ARCH_DIR/snapshot.zip
+# ZIPFILE=snapshot.zip
 
 # uncomment the following if you have no virtualenv:
-ENVDIR=env
+ENVDIR=$PROJECT_DIR/env
 
 # make new files writable for other group members:
 umask 0007
 
 if [ -f $ZIPFILE ]
   then
-  mv $ZIPFILE `date -r $ZIPFILE +%Y%m%d_%H%M.zip`
-  # echo "Sorry, there is already a file $ZIPFILE. Delete it yourself if you dare."
-  # exit -1
+  ARCFILE=`date +$ARCH_DIR/%Y%m%d_%H%M.zip -r $ZIPFILE`
+  mv $ZIPFILE $ARCFILE
+  echo Moved $ZIPFILE to $ARCFILE
 fi
+
+cd $PROJECT_DIR
 
 if [ -d $ENVDIR ]
   then
@@ -54,3 +70,7 @@ zip -r $ZIPFILE fixtures
 zip -r $ZIPFILE media/webdav
 zip -r $ZIPFILE media/beid
 zip $ZIPFILE *.py *.sh
+
+# delete all files older than 60 days in ARCHDIR:
+find $ARCH_DIR -maxdepth 1 -depth -mtime +60 -delete
+
