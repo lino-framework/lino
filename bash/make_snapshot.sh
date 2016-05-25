4ff06d8cac21c16ce43d393a36070665d3789514
Luc Saffre <luc.saffre@gmail.com>
2016-05-25 17:57:00 +0300

    http://luc.lino-framework.org/blog/2016/0525.html

--- bash/make_snapshot.sh
+++ bash/make_snapshot.sh
@@ -9,45 +9,48 @@
 
 set -e  # exit on error
 
-# edit the following line if this is also to be called from
-# /etc/cron.d/lino_backup where cwd is not set:
-# cd /path/to/project/dir
+# YOU MUST edit the following lines:
+# WARNING: all files older than 60 days in ARCHDIR will be deleted
+# without confirmation
 
 PROJECT_DIR=/path/to/myproject
 ARCH_DIR=/var/backups/lino/myproject
+ENVDIR=$PROJECT_DIR/env
 
-if [ ! -d "$PROJET_DIR" ]; then
-  echo $PROJET_DIR does not exist!
+# edit the following lines if you also want to include a MySQL dump
+# MYSQL_USERNAME=
+# MYSQL_PASSWORD=
+# MYSQL_DBNAME=
+
+# Directory where to put the temporary snapshot files.
+# This is relative to PROJECT_DIR 
+# WARNING: everything in this directory will be deleted without confirmation
+SNAPSHOTDIR=snapshot
+
+# name of target zip file to be created:
+# This is relative to PROJECT_DIR 
+ZIPFILE=snapshot.zip
+
+if [ ! -d "$PROJECT_DIR" ]; then
+  echo "PROJECT_DIR ($PROJECT_DIR) does not exist!"
   exit -1
 fi
 
-# edit the following lines if you also want to include a MySQL dump
-MYSQL_USERNAME=
-MYSQL_PASSWORD=
-MYSQL_DBNAME=
+if [ ! -d "$ARCH_DIR" ]; then
+  echo "ARCH_DIR ($ARCH_DIR) does not exist!"
+  exit -1
+fi
 
-# Directory where to put the temporary snapshot files.
-# WARNING: everything in this directory will be deleted without confirmation
-SNAPSHOTDIR=$PROJECT_DIR/snapshot
+umask 0007 # make new files writable for other group members
 
-# name of target zip file to be created:
-ZIPFILE=$ARCH_DIR/snapshot.zip
-# ZIPFILE=snapshot.zip
-
-# uncomment the following if you have no virtualenv:
-ENVDIR=$PROJECT_DIR/env
-
-# make new files writable for other group members:
-umask 0007
+cd $PROJECT_DIR
 
 if [ -f $ZIPFILE ]
   then
   ARCFILE=`date +$ARCH_DIR/%Y%m%d_%H%M.zip -r $ZIPFILE`
+  echo "Move existing $ZIPFILE to $ARCFILE"
   mv $ZIPFILE $ARCFILE
-  echo Moved $ZIPFILE to $ARCFILE
 fi
-
-cd $PROJECT_DIR
 
 if [ -d $ENVDIR ]
   then
@@ -62,9 +65,12 @@ fi
 
 python manage.py dump2py $SNAPSHOTDIR
 pip freeze > $SNAPSHOTDIR/requirements.txt
-if $MYSQL_USERNAME ; then
-  mysqldump -u $MYSQL_USERNAME --password=$MYSQL_PASSWORD $MYSQL_DBNAME > $SNAPSHOTDIR/dump.sql
+if [ "$MYSQL_USERNAME" != "" ] ; then
+  echo -n "Writing MySQL dump..."
+  mysqldump -u "$MYSQL_USERNAME" --password="$MYSQL_PASSWORD" "$MYSQL_DBNAME" > $SNAPSHOTDIR/dump.sql
+  echo " done"
 fi
+echo "Writing $ZIPFILE..."
 zip -r $ZIPFILE $SNAPSHOTDIR
 zip -r $ZIPFILE fixtures
 zip -r $ZIPFILE media/webdav
@@ -72,5 +78,5 @@ zip -r $ZIPFILE media/beid
 zip $ZIPFILE *.py *.sh
 
 # delete all files older than 60 days in ARCHDIR:
-find $ARCH_DIR -maxdepth 1 -depth -mtime +60 -delete
+find $ARCH_DIR -maxdepth 1 -name '*.zip' -depth -mtime +60 -delete
 
