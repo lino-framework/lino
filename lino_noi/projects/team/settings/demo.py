affe31b06002e01bc34ef4603cec0f4e88d00ee0
Luc Saffre <luc.saffre@gmail.com>
2016-06-15 05:25:09 +0300

    http://luc.lino-framework.org/blog/2016/0615.html

--- lino_noi/projects/team/settings/demo.py
+++ lino_noi/projects/team/settings/demo.py
@@ -10,12 +10,12 @@ class Site(Site):
 
     languages = "en de fr"
 
-    # default_ui = 'lino_extjs6.extjs6'
+    default_ui = 'lino_extjs6.extjs6'
 
-    # def get_apps_modifiers(self, **kw):
-    #     kw = super(Site, self).get_apps_modifiers(**kw)
-    #     kw.update(tinymce=None)
-    #     return kw
+    def get_apps_modifiers(self, **kw):
+        kw = super(Site, self).get_apps_modifiers(**kw)
+        kw.update(tinymce=None)
+        return kw
 
     def setup_plugins(self):
         """Change the default value of certain plugin settings.
@@ -30,6 +30,16 @@ class Site(Site):
         self.plugins.excerpts.configure(responsible_user='jean')
 
 SITE = Site(globals())
+# SITE.plugins.extjs6.configure(theme_name='theme-classic')
+# SITE.plugins.extjs6.configure(theme_name='theme-classic-sandbox')
+SITE.plugins.extjs6.configure(theme_name='theme-aria')
+# SITE.plugins.extjs6.configure(theme_name='theme-grey')
+# SITE.plugins.extjs6.configure(theme_name='theme-crisp')
+# SITE.plugins.extjs6.configure(theme_name='theme-crisp-touch')
+# SITE.plugins.extjs6.configure(theme_name='theme-neptune')
+# SITE.plugins.extjs6.configure(theme_name='theme-neptune-touch')
+# SITE.plugins.extjs6.configure(theme_name='theme-triton')
+# SITE.plugins.extjs6.configure(theme_name='ext-theme-neptune-lino')
 
 DEBUG = True
 
