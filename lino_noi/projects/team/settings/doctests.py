63d078eac225555320c0ad0dd2b69b2f6480a6e0
Luc Saffre <luc.saffre@gmail.com>
2016-08-02 10:16:17 +0300

    http://luc.lino-framework.org/blog/2016/0802.html

--- lino_noi/projects/team/settings/doctests.py
+++ lino_noi/projects/team/settings/doctests.py
@@ -3,7 +3,7 @@ SITE = Site(
     globals(),
     remote_user_header='REMOTE_USER')
 DEBUG = True
-SITE.appy_params.update(raiseOnError=True)
-SITE.appy_params.update(pythonWithUnoPath='/usr/bin/python3')
+# SITE.appy_params.update(raiseOnError=True)
+# SITE.appy_params.update(pythonWithUnoPath='/usr/bin/python3')
 SITE.default_build_method = "appyodt"
 SITE.webdav_url = '/'
