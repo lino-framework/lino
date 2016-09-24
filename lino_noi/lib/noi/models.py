52bc30fff0b7dba47848d952ed066a3d10979dea
Luc Saffre <luc.saffre@gmail.com>
2016-09-24 14:25:52 +0300

    Py3 syntax error

--- lino_noi/lib/noi/models.py
+++ lino_noi/lib/noi/models.py
@@ -23,6 +23,8 @@ Defines a handler for :data:`lino.modlib.smtpd.signals.mail_received`.
 
 """
 
+from __future__ import print_function
+
 from email.parser import Parser
 
 from lino.api import dd
@@ -32,14 +34,14 @@ from lino.modlib.smtpd.signals import mail_received
 @dd.receiver(mail_received)
 def process_message(sender=None, peer=None, mailfrom=None,
                     rcpttos=None, data=None, **kwargsg):
-    print 'Receiving message from:', peer
-    print 'Message addressed from:', mailfrom
-    print 'Message addressed to  :', rcpttos
-    print 'Message length        :', len(data)
-    msg = Parser().parsestr(data)
-    print 'To: %s' % msg['to']
-    print 'From: %s' % msg['from']
-    print 'Subject: %s' % msg['subject']
+    print('Receiving message from:', peer)
+    print('Message addressed from:', mailfrom)
+    print('Message addressed to  :', rcpttos)
+    print('Message length        :', len(data))
+    msg = Parser().parsestr(data))
+    print('To: %s' % msg['to'])
+    print('From: %s' % msg['from'])
+    print('Subject: %s' % msg['subject'])
     return None
 
 
