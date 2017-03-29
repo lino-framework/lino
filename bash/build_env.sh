263b8120a135b25f67141b7cb3ce146b2f5178db
Tonis piip <tonis.piip@gmail.com>
2017-03-29 22:52:06 +0300

    Added pip install ipython and mock to build script

--- bash/build_env.sh
+++ bash/build_env.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 virtualenv 2.7
 . 2.7/bin/activate
 mkdir src
@@ -15,19 +16,20 @@ for comm in https://github.com/lsaffre/commondata \
 	    https://github.com/lsaffre/commondata-be \
 	    https://github.com/lsaffre/commondata-ee \
 	    https://github.com/lsaffre/commondata-eg \
-            git@github.com:cylonoven/django-mailbox.git \
-	    https://github.com/lsaffre/sphinx.git
+            git@github.com:cylonoven/django-mailbox.git
 do
     git clone $comm
 done
 
 
-for i in sphinx atelier lino xl noi django-mailbox extjs6 commondata commondata-be commondata-ee commondata-eg cosi voga presto welfare avanti book
+for i in atelier lino xl noi django-mailbox extjs6 commondata commondata-be commondata-ee commondata-eg cosi voga presto welfare avanti book
 do
     pip install -e $i
 done
 
-sudo apt-get install libreoffice python3-uno
+pip install mock ipython
 
-sudo apt-get install build-essential libssl-dev libffi-dev python-dev mock
+sudo apt-get install libreoffice python3-uno python-pygraphviz
+
+sudo apt-get install build-essential libssl-dev libffi-dev python-dev
 
