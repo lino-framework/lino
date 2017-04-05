46657d00ca39114f01668843dc08969650327940
Luc Saffre <luc.saffre@gmail.com>
2017-04-05 09:23:54 +0300

    #1673 Who is online?

---
+++ lino/modlib/blacklist/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2017 Luc Saffre
+# License: BSD (see file COPYING for details)
+
+"""I started this plugin just out of curiosity.  When finished it
+would do the same as ipdict except that it uses the Django sessions to
+store and display these data. Advantage would be that we don't need to
+worry what happens when a site has so many http connections that
+storing an IPRecord for each of them will cause a memory overflow...
+
+
+.. autosummary::
+   :toctree:
+
+    models
+
+"""
+
+from lino.api import ad
+
+
+class Plugin(ad.Plugin):
+    "See :doc:`/dev/plugins`."
+
+    needs_plugins = ['django.contrib.sessions']
+
+    def setup_site_menu(config, site, profile, m):
+        m.add_action(site.modules.blacklist.Sessions)
