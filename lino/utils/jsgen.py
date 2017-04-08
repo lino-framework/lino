3fadfa2d907fca1b9ce152d0b80e280146757f10
Luc Saffre <luc.saffre@gmail.com>
2017-04-08 04:35:52 +0300

    http://luc.lino-framework.org/blog/2017/0408.html

--- lino/utils/jsgen.py
+++ lino/utils/jsgen.py
@@ -448,10 +448,11 @@ def py2js(v):
     if isinstance(v, types.GeneratorType):
         return "".join([py2js(x) for x in v])
     if etree.iselement(v):
+        # return json.dumps(force_text(etree.tostring(v)))
         try:
             return json.dumps(force_text(etree.tostring(v)))
         except Exception as e:
-            raise Exception("Failed to render {!r} : {}".format(v, e))
+            return "Failed to render {!r} : {}".format(v, e)
 
     # if type(v) is types.GeneratorType:
         # raise Exception("Please don't call the generator function yourself")
