c33971bda0db7f14690973d152a52460fa2f274f
Tonis piip <tonis.piip@gmail.com>
2017-04-25 20:50:34 +0300

    Updated bash update env script

--- bash/build_env.sh
+++ bash/build_env.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-virtualenv 2.7
-. 2.7/bin/activate
+
+
+# Clone sources
+
 mkdir src
 cd src
 
-
-for pro in atelier lino xl cosi noi book voga welfare avanti extjs6
+for pro in atelier lino xl cosi noi book voga welfare avanti extjs6 presto
 
 do
     git clone git@github.com:lino-framework/$pro.git
@@ -22,12 +23,23 @@ do
 done
 
 
-for i in atelier lino xl noi django-mailbox extjs6 commondata commondata-be commondata-ee commondata-eg cosi voga welfare avanti book
+cd ..
+
+virtualenv -p python2 2.7
+virtualenv -p python3 3.5
+
+cd src
+
+for PY in 3.5 2.7
+do . ../$PY/bin/activate
+
+for i in atelier lino xl noi django-mailbox extjs6 commondata commondata-be commondata-ee commondata-eg cosi voga welfare avanti book presto
 do
     pip install -e $i
 done
 
-pip install mock ipython
+pip install mock ipython radicale
+done
 
 sudo apt-get install libreoffice python3-uno python-pygraphviz
 
