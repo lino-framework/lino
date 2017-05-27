d21665d2259feff980dea6d08cbbeafeccf7d8c0
tonis piip <tonis@linovm>
2017-05-27 10:22:40 -0400

    Updated build_env script to include python dev3

--- bash/build_env.sh
+++ bash/build_env.sh
@@ -22,6 +22,7 @@ do
     git clone $comm
 done
 
+sudo apt-get install libreoffice python3-uno python-pygraphviz virtualenv build-essential libssl-dev libffi-dev python-dev python3-dev gcc
 
 cd ..
 
@@ -33,10 +34,10 @@ cd src
 for PY in 3.5 2.7
 do . ../$PY/bin/activate
 
-pip -U pip
-pip -U setuptools
+pip install -U pip
+pip install -U setuptools
 
-for i in atelier lino xl noi django-mailbox extjs6 commondata commondata-be commondata-ee commondata-eg cosi voga welfare vilma avanti book presto care riche tera
+for i in atelier lino xl noi django-mailbox extjs6 commondata commondata-be commondata-ee commondata-eg cosi voga welfare vilma avanti presto care riche tera book
 do
     pip install -e $i
 done
@@ -44,7 +45,4 @@ done
 pip install mock ipython radicale
 done
 
-sudo apt-get install libreoffice python3-uno python-pygraphviz
-
-sudo apt-get install build-essential libssl-dev libffi-dev python-dev
 
