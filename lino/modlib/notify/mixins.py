e001280c05f647382fb67cdc4f5ef0b7766263e5
Tonis piip <tonis.piip@gmail.com>
2017-06-20 00:20:27 +0300

    Hard coded exception for _user_cache

--- lino/modlib/notify/mixins.py
+++ lino/modlib/notify/mixins.py
@@ -62,7 +62,7 @@ class ChangeObservable(dd.Model):
                 _("{user} created {what}").format(**ctx), ".")]
             elems += list(self.get_change_info(ar, cw))
         else:
-            items = list(cw.get_updates_html(self.change_watcher_spec.ignored_fields))
+            items = list(cw.get_updates_html(["_user_cache"]))
             if len(items) == 0:
                 return
             elems = []
