d46767944d0809126d3bda27bd1bfc85733cf665
Luc Saffre <luc.saffre@gmail.com>
2018-01-12 08:02:11 +0200

    http://luc.lino-framework.org/blog/2018/0112.html

--- lino/setup_info.py
+++ lino/setup_info.py
@@ -151,6 +151,7 @@ lino.modlib.checkdata
 lino.modlib.checkdata.fixtures
 lino.modlib.checkdata.management
 lino.modlib.checkdata.management.commands
+lino.modlib.openui5
 lino.modlib.printing
 lino.modlib.restful
 lino.modlib.smtpd
