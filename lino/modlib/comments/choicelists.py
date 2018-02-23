85f3e797b62d8e6417add9acfe1df67cb2a7737d
Luc Saffre <luc.saffre@gmail.com>
2018-02-23 09:16:15 +0200

    #2327 remove Comment.published, restore ChangeObservable

--- lino/modlib/comments/choicelists.py
+++ lino/modlib/comments/choicelists.py
@@ -4,7 +4,7 @@
 
 from lino.api import dd, rt, _
 from lino.modlib.system.choicelists import ObservedEvent
-from .roles import CommentsUser
+# from .roles import CommentsUser
 
 class ObservedTime(ObservedEvent):
 
@@ -32,42 +32,42 @@ def add(*args):
     CommentEvents.add_item_instance(ObservedTime(*args))
 add('10', 'created', _("Created"))
 add('20', 'modified', _("Modified"))
-add('30', 'published', _("Published"))
+#add('30', 'published', _("Published"))
 
 
 
-class PublishComment(dd.Action):
-    sort_index = 100
-    label = _("Publish")
-    show_in_workflow = True
-    show_in_bbar = False
-    required_roles = dd.login_required(CommentsUser)
+# class PublishComment(dd.Action):
+#     sort_index = 100
+#     label = _("Publish")
+#     show_in_workflow = True
+#     show_in_bbar = False
+#     required_roles = dd.login_required(CommentsUser)
 
     
-    def run_from_ui(self, ar, **kw):
-        for obj in ar.selected_rows:
-            obj.do_publish(ar)
-        ar.success(
-            _("{0} comments published.").format(
-                len(ar.selected_rows)), refresh_all=True)
+#     def run_from_ui(self, ar, **kw):
+#         for obj in ar.selected_rows:
+#             obj.do_publish(ar)
+#         ar.success(
+#             _("{0} comments published.").format(
+#                 len(ar.selected_rows)), refresh_all=True)
 
-class PublishAllComments(PublishComment):
-    label = _("Publish all")
-    show_in_workflow = False
-    show_in_bbar = True
-    select_rows = False
-    default_format = 'ajax'
+# class PublishAllComments(PublishComment):
+#     label = _("Publish all")
+#     show_in_workflow = False
+#     show_in_bbar = True
+#     select_rows = False
+#     default_format = 'ajax'
     
-    def run_from_ui(self, ar, **kw):
-        n = ar.get_total_count()
-        def ok(ar):
-            for obj in ar:
-                obj.do_publish(ar)
-            ar.success(
-                _("{0} comments published.").format(n), refresh_all=True)
+#     def run_from_ui(self, ar, **kw):
+#         n = ar.get_total_count()
+#         def ok(ar):
+#             for obj in ar:
+#                 obj.do_publish(ar)
+#             ar.success(
+#                 _("{0} comments published.").format(n), refresh_all=True)
 
-        ar.confirm(
-            ok, _("This will publish {} comments.").format(n),
-            _("Are you sure?"))
+#         ar.confirm(
+#             ok, _("This will publish {} comments.").format(n),
+#             _("Are you sure?"))
 
         
