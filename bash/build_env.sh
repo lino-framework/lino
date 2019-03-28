6dafbc81a8b96b9861fa5866d2d671ce5842b7d5
Tonis Piip <Tonis@eggo.org>
2019-03-28 13:33:39 +0200

    Update build_env.sh script for py3 from fresh install of linux

--- bash/build_env.sh
+++ bash/build_env.sh
@@ -6,7 +6,7 @@
 mkdir src
 cd src
 
-for pro in atelier lino xl cosi noi book voga welfare care avanti extjs6 presto vilma tera riche
+for pro in atelier lino xl cosi noi book voga welfare care avanti extjs6 presto vilma tera riche react openui5 etgen
 
 do
     git clone git@github.com:lino-framework/$pro.git
@@ -17,32 +17,33 @@ for comm in https://github.com/lsaffre/commondata \
 	    https://github.com/lsaffre/commondata-be \
 	    https://github.com/lsaffre/commondata-ee \
 	    https://github.com/lsaffre/commondata-eg \
-            git@github.com:cylonoven/django-mailbox.git
+        git@github.com:cylonoven/django-mailbox.git
 do
     git clone $comm
 done
 
+# appy
+sudo apt-get install subversion
+svn checkout https://svn.forge.pallavi.be/appy-dev
+pip install -e appy-dev/dev1
+
+
 sudo apt-get install libreoffice python3-uno python-pygraphviz virtualenv build-essential libssl-dev libffi-dev python-dev python3-dev gcc
 
-cd ..
-
-virtualenv -p python2 2.7
-virtualenv -p python3 3.5
-
-cd src
-
-for PY in 3.5 2.7
-do . ../$PY/bin/activate
+# Notification service
+sudo apt-get install redis redis-server
 
 pip install -U pip
 pip install -U setuptools
+pip install mock ipython
 
-for i in atelier lino xl noi django-mailbox extjs6 commondata commondata-be commondata-ee commondata-eg cosi voga welfare vilma avanti presto care riche tera book
+# order is important.
+for i in atelier/ lino/ xl/ noi/ django-mailbox/ extjs6/ commondata/ commondata-be/ commondata-ee/ commondata-eg/ \
+         cosi/ voga/ welfare/ vilma/ avanti/ presto/ care/ riche/ tera/ react/ openui5/ book/ etgen/
 do
     pip install -e $i
 done
 
-pip install mock ipython radicale
-done
+
 
 
