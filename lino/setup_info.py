dbe8c4089d2995842bd17ddbc6139f82040fb288
Luc Saffre <luc.saffre@gmail.com>
2019-07-01 03:30:36 +0300

    http://luc.lino-framework.org/blog/2019/0701.html

--- lino/setup_info.py
+++ lino/setup_info.py
@@ -20,7 +20,7 @@ PY3 = sys.version_info[0] == 3
 
 SETUP_INFO = dict(
     name='lino',
-    version='19.6.3',
+    version='19.7.0',
     description="A framework for writing desktop-like web applications "
                 "using Django and ExtJS or React",
     license='BSD-2-Clause',
