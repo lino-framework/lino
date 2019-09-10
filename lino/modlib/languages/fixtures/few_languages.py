a3b3147ac192b68b2fc1b5343cf09311cae8064a
Luc Saffre <luc.saffre@gmail.com>
2019-09-10 15:39:02 +0300

    few_languages now also sets iso2

--- lino/modlib/languages/fixtures/few_languages.py
+++ lino/modlib/languages/fixtures/few_languages.py
@@ -1,23 +1,28 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2009-2014 Rumma & Ko Ltd
+# Copyright 2009-2019 Rumma & Ko Ltd
 # License: BSD (see file COPYING for details)
 """
-Adds an arbitrary selection of a few demo languages.
+Adds an arbitrary selection of a few languages.
 """
 
 from __future__ import unicode_literals
 
 from django.utils.translation import ugettext_lazy as _
-from lino.utils.instantiator import Instantiator
+# from lino.utils.instantiator import Instantiator
 from lino.api import dd, rt
 
 
 def objects():
 
-    Language = Instantiator('languages.Language', "id").build
+    # Language = Instantiator('languages.Language', "id").build
 
-    yield Language('ger', **dd.str2kw('name', _("German")))
-    yield Language('fre', **dd.str2kw('name', _("French")))
-    yield Language('eng', **dd.str2kw('name', _("English")))
-    yield Language('dut', **dd.str2kw('name', _("Dutch")))
-    yield Language('est', **dd.str2kw('name', _("Estonian")))
+    def language(pk, iso2, name):
+        kw = dict(id=pk, iso2=iso2)
+        kw.update(dd.str2kw('name', name))
+        return rt.models.languages.Language(**kw)
+
+    yield language('ger', 'de', _("German"))
+    yield language('fre', 'fr', _("French"))
+    yield language('eng', 'en', _("English"))
+    yield language('dut', 'nl', _("Dutch"))
+    yield language('est', 'et', _("Estonian"))
