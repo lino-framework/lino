e39bc3ae4d93e075fc13b6e1ad231120175c7747
Luc Saffre <luc.saffre@gmail.com>
2020-05-21 16:18:21 +0300

    release to pypi

--- lino/setup_info.py
+++ lino/setup_info.py
@@ -18,7 +18,7 @@ PY3 = sys.version_info[0] == 3
 
 SETUP_INFO = dict(
     name='lino',
-    version='20.5.0',
+    version='20.5.1',
     description="A framework for writing desktop-like web applications "
                 "using Django and ExtJS or React",
     license='BSD-2-Clause',
