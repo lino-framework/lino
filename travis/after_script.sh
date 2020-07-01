0f8be71af9dd3c38b3ff0666d21c567e9ed1362f
Luc Saffre <luc.saffre@gmail.com>
2020-07-01 12:29:46 +0300

    fix #3706 lino repo gets red when book fails

--- travis/after_script.sh
+++ travis/after_script.sh
@@ -2,6 +2,7 @@
 # Thanks to https://docs.travis-ci.com/user/job-lifecycle/
 set -ev
 # echo "TRAVIS_BRANCH=$TRAVIS_BRANCH TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
+echo TRAVIS_API_TOKEN=$TRAVIS_API_TOKEN
 MSG="Auto test triggered by repo `basename $(git rev-parse --show-toplevel)` commit `git rev-parse HEAD`"
 ./travis/trigger_build.sh "lino-framework/react" "$MSG" "$TRAVIS_API_TOKEN"
 ./travis/trigger_build.sh "lino-framework/welfare" "$MSG" "$TRAVIS_API_TOKEN"
