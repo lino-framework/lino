4114c039a676537ffbd036776bfd0b137fc78953
Luc Saffre <luc.saffre@gmail.com>
2020-07-01 11:53:18 +0300

    fix #3706 lino repo gets red when book fails

---
+++ travis/after_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Thanks to https://docs.travis-ci.com/user/job-lifecycle/
+set -ev
+# echo "TRAVIS_BRANCH=$TRAVIS_BRANCH TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
+MSG="Auto test triggered by repo `basename $(git rev-parse --show-toplevel)` commit `git rev-parse HEAD`"
+./travis/trigger_build.sh "lino-framework/react" "$MSG" "$TRAVIS_API_TOKEN"
+./travis/trigger_build.sh "lino-framework/welfare" "$MSG" "$TRAVIS_API_TOKEN"
+./travis/trigger_build.sh "lino-framework/presto" "$MSG" "$TRAVIS_API_TOKEN"
+./travis/trigger_build.sh "lino-framework/book" "$MSG" "$TRAVIS_API_TOKEN"
--- travis_after_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Thanks to https://docs.travis-ci.com/user/job-lifecycle/
-set -ev
-# echo "TRAVIS_BRANCH=$TRAVIS_BRANCH TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
-MSG="Auto test triggered by repo `basename $(git rev-parse --show-toplevel)` commit `git rev-parse HEAD`"
-bash/trigger_build.sh "lino-framework/react" "$MSG" "$TRAVIS_API_TOKEN"
-bash/trigger_build.sh "lino-framework/welfare" "$MSG" "$TRAVIS_API_TOKEN"
-bash/trigger_build.sh "lino-framework/presto" "$MSG" "$TRAVIS_API_TOKEN"
-bash/trigger_build.sh "lino-framework/book" "$MSG" "$TRAVIS_API_TOKEN"
